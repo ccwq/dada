5ee7a5faf00f4988eee8e30071d8876fc40cb18a
ccwq <scwqd@qq.com>
2015-01-05 15:37:10 +0800

    Initial commit

---
+++ html/data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'dada_db';
+$cfg_dbuser = 'dada';
+$cfg_dbpwd = 'ccwq2ccwq';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
